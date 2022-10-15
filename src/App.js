876605abfaf2c407434c6f1f06ded3047477eb8d
kaicampbell1738 <kaicampbell1738@gmail.com>
2022-10-14 17:34:54 -0700

    Added new text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+  const api = {
+      key: "8e86d819c113a26118df4780eb390424",
+      base: "https://api.openweathermap.org/data/3.0/"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="">
+          New York
+          60 Degrees Fahrenheit
+      </div>
   );
 }
 
