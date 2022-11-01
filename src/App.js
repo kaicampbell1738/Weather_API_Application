0293f8ece3c80ae1a5753cc32ea2774985ec7856
kaicampbell1738 <kaicampbell1738@gmail.com>
2022-11-01 03:56:12 -0700

    Added some template text for future usage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
-  const api = {
-      key: "8e86d819c113a26118df4780eb390424",
-      base: "https://api.openweathermap.org/data/3.0/"
-  }
+  
+    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=8e86d819c113a26118df4780eb390424'
 
   return (
-      <div className="">
-          New York
-          60 Degrees Fahrenheit
+      <div className="App">
+         <div className="container">
+            <div className="top">
+               <div className="location">
+                  <p>Dallas</p>
+               </div>
+               <div className="temp">
+                  <h1>60F</h1>
+               </div>
+               <div className="decription">
+                  <p>Clouds</p>
+               </div>
+            </div>
+         <div className="bottom"></div>
+            <div className="feels">
+               <p>65F</p>
+              </div>
+            <div className="humidity">
+               <p>20%</p>
+            </div>
+            <div className="wind">
+                <p>12 MPH</p>
+            </div>
+         </div>
       </div>
   );
 }
