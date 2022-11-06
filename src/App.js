11c354d15e2be7e8d38778cafc6b8be7b311661d
kaicampbell1738 <kaicampbell1738@gmail.com>
2022-11-06 04:47:48 -0800

    Added Background and modfified layout

--- src/App.js
+++ src/App.js
@@ -6,30 +6,31 @@ function App() {
     // const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=8e86d819c113a26118df4780eb390424'
 
   return (
-      <div className="App">
-         <div className="container">
-            <div className="top">
-               <div className="location">
-                  <p>Dallas</p>
-               </div>
-               <div className="temp">
-                  <h1>60F</h1>
-               </div>
-               <div className="decription">
-                  <p>Clouds</p>
-               </div>
+      <div className="app">
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>Dallas</p>
             </div>
-         <div className="bottom"></div>
+            <div className="temp">
+              <h1>60F</h1>
+            </div>
+            <div className="decription">
+              <p>Clouds</p>
+            </div>
+          </div>
+          <div className="bottom">
             <div className="feels">
-               <p>65F</p>
-              </div>
+              <p>65F</p>
+            </div>
             <div className="humidity">
-               <p>20%</p>
+              <p>20%</p>
             </div>
             <div className="wind">
-                <p>12 MPH</p>
+              <p>12 MPH</p>
             </div>
-         </div>
+          </div>
+        </div>
       </div>
   );
 }
